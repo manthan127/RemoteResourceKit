a36025b1d632d544197ac9dbdec9b5ef7beea24d
manthan127 <gevariyamanthan8@gmail.com>
2026-02-13 22:52:04 +0530

    minor change

--- Sources/RemoteResourceKit/Extensions/HelperExtensions.swift
+++ Sources/RemoteResourceKit/Extensions/HelperExtensions.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension FileManager {
     func fileExists(at path: URL) -> Bool {
-        if #available(iOS 16.0, *) {
+        if #available(iOS 16.0, macOS 13.0, *) {
             fileExists(atPath: path.path(percentEncoded: true))
         } else {
             fileExists(atPath: path.path)
